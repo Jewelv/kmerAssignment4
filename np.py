2c8984423ad1eb90a3fe2362f2b5b5b6295a5911
Jewelv <68551911+Jewelv@users.noreply.github.com>
2021-05-12 17:36:30 -0400

    Update np.py

--- np.py
+++ np.py
@@ -5,9 +5,7 @@ import numpy as np
 import pandas as pd
 import argparse
 
-#python starts with 0 value
-
-def unique(ary): 
+def unique(ary):
   list = []
   i=0
   x= np.array(ary)
@@ -15,11 +13,10 @@ def unique(ary):
    list.append(np.unique(x[i]))
   return list
 
-
 def window(string):
   j = []
   k = 0
-  l = 0
+  l = 1
   while k < len(string) + 1:
     d = [string[i:i+k] for i in range(len(string)-l+1)]
     j.append(d)
@@ -27,8 +24,6 @@ def window(string):
     l+=1
   return j
 
-#generation of the observed kmer values from the string
-#
 def get_obs(list):
   get_list = []
   for x_list in list:
@@ -36,10 +31,6 @@ def get_obs(list):
     counter = len(x_list)
     get_list.append(counter)
   return get_list
-
-#function to get the possible kmer values. 
-#k must be less or equal to the string sequence length
-#this will generate all possible kmer value for the string
 
 def get_pos(string):
   j= []
@@ -61,7 +52,6 @@ def get_pos(string):
    k+=1
   return j
 
-#generation of the dataframe in order of columns and obtains the previously generated data
 def get_df(string,get_list,pos):
   kmer = []
   for i in range(1,len(string)+1):
@@ -71,13 +61,15 @@ def get_df(string,get_list,pos):
   df.at['total','Pos'] = df['Pos'].sum()
   return df
 
-#function to obtain linguistic complexity through the summarization of the observed kmers and division of the possible kmers
 def get_lc(df):
   lc = df['Obs'].sum()/df['Pos'].sum()
   return lc
+  
 
-def main():
-  fn = open("dna.txt","r+") #edit to your text file
+
+
+def main(args):
+  fn = open("dna.txt","r+")
   file = [seq.strip() for seq in fn]
   for seq in file:
     ary = window(seq)
@@ -87,10 +79,19 @@ def main():
     pos = get_pos(seq)
     df1 = get_df(seq,get_list,pos)
     df2 = df1['Obs'].sum()/df1['Pos'].sum()
+    #lc = get_lc(df)
     print(df1)
     print(df2)
     f= open("kmerAssignmentOutput.txt", 'a')
     f.write(str(df1)+ str(df2)+' ')
     f.close()
   fn.close()
-main()
+
+
+if __name__ == '__main__': 
+  parser = argparse.ArgumentParser()
+  parser.add_argument('dna', type=argparse.FileType('r'))
+  #parser.add_argument('kmer', type = int)
+  args = parser.parse_args()
+  main(args)
+
